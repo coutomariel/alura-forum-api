ca4a40a207e382d0315327100d645a6a21ba7c80
Mariel Couto <coutomariel@gmail.com>
2021-05-31 08:19:46 -0300

    Ajuste de package

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.FormLogin;
 import br.com.alura.forum.controller.dto.TokenResponseDto;
-import br.com.alura.forum.security.TokenService;
 
 @RestController
 @RequestMapping
